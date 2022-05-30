f95e9639cf0f111c2c3644f6dd010c866e3090c3
David Tonoyan <davidtonoyan10@me.com>
2022-05-30 22:37:56 +0300

    Update constraits

--- simple_Game/Controller/ViewController.swift
+++ simple_Game/Controller/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var gameFieldView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gameFieldView.layer.borderWidth = 1
+        gameFieldView.layer.borderColor = UIColor.red.cgColor
+        gameFieldView.layer.cornerRadius = 10
     }
     
-    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        timeLabel.text = "Time: \(sender.value) sec"
+    }
 }
 
